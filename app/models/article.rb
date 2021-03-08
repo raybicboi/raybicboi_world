60f0cb2d16e6382038a05cb46af9ad433ee41e72
abhidevarapalli <adevarap@cmu.edu>
2021-03-08 12:29:56 -0500

    added validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ApplicationRecord
+
+    #relationships
+    belongs_to :category
+
+    # scopes
+    scope :alphabetical, -> { order('title') }
+    scope :active, 			 -> { where('active = ?', true) }
+
+    validates_presence_of :title, :content
+
 end
